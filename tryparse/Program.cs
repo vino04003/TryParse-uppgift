b1e696cfa416f44b896998628e2dc6e02a87b8a4
vino04003 <89254939+vino04003@users.noreply.github.com>
2022-11-24 12:27:22 +0100

    Add files via upload

---
+++ tryparse/Program.cs
@@ -0,0 +1,15 @@
+﻿    
+    Console.WriteLine("Skriv ett nummer: ");
+
+    int number;
+
+        if(int.TryParse(Console.ReadLine(), out number))
+        {
+            Console.WriteLine("nummret var " + number);
+        }
+        else
+        {
+            Console.WriteLine("Inget nummer");
+        }
+    Console.ReadLine();
+
